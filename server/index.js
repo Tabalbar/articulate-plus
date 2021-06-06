312c5defad5249d15ed47ed22931145ce089cf32
Tabalbar <35385849+Tabalbar@users.noreply.github.com>
2021-06-05 15:27:21 -1000

    make it better

---
+++ server/index.js
@@ -0,0 +1,30 @@
+// server/index.js
+
+const express = require("express");
+
+const PORT = process.env.PORT || 6000;
+
+const app = express();
+
+const path = require('path');
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
