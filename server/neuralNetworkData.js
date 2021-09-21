820d052e0a4416bf1728858775235e9b5f3ad6e8
Tabalbar <35385849+Tabalbar@users.noreply.github.com>
2021-09-20 22:13:25 -1000

    moved neural network data

---
+++ server/neuralNetworkData.js
@@ -0,0 +1,60 @@
+module.exports = {
+  queries: [
+    { query: "show me the distribution of nominal", chartType: "bar" },
+    { query: "show me a graph with nominal and nominal", chartType: "bar" },
+    { query: "show me a chart of nominal and nominal", chartType: "bar" },
+    {
+      query: "I want to see the comparison of nominal over time",
+      chartType: "line",
+    },
+    { query: "show me nominal by date", chartType: "line" },
+    {
+      query: "show me the comparison of nominal over temporal",
+      chartType: "line",
+    },
+    {
+      query: "Show me the temporal over the year of nominal and quantitative",
+      chartType: "line",
+    },
+    {
+      query: "I want to see quantitative by quantitative",
+      chartType: "scatter",
+    },
+    {
+      query: "show me the relationship of quantitative and quantitative",
+      chartType: "scatter",
+    },
+    { query: "what is the quantitative of quantitative", chartType: "scatter" },
+    {
+      query: "show me the correlation of quantiative and quantitative",
+      chartType: "heatmap",
+    },
+    { query: "show me quantitative by quantitative", chartType: "heatmap" },
+  ],
+  answers: [{ chartType: "   " }],
+};
+
+manager.addDocument(
+  "en",
+  "I want to see quantitative by quantitative",
+  "scatter"
+);
+manager.addDocument(
+  "en",
+  "show me the relationship of quantitative and quantitative",
+  "scatter"
+);
+manager.addDocument(
+  "en",
+  "what is the quantitative of quantitative",
+  "scatter"
+);
+manager.addAnswer("en", "scatter", "scatter");
+
+manager.addDocument(
+  "en",
+  "show me the correlation of quantiative and quantitative",
+  "heatmap"
+);
+manager.addDocument("en", "show me quantitative by quantitative", "heatmap");
+manager.addAnswer("en", "heatmap", "heatmap");
